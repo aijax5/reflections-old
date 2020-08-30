c13e784c8b2a3d34586e6bff3b66bef040222f4c
aijax5 <mdajaaz1999@gmail.com>
2020-08-30 23:38:02 +0530

    typo

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import Post
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello this is welcome page')
+    return HttpResponse('Hello this is the landing page')
 
 def individual_post(request):
     recent_post = Post.objects.get(id__exact=2)
